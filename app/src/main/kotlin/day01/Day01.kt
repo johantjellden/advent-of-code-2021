b8f4c747d6b2a54ed0496d79ae2f8923aea29676
Johan Tjelldén <johan@hedvig.com>
2021-12-01 22:21:09 +0100

    Day one complete

--- app/src/main/kotlin/day01/Day01.kt
+++ app/src/main/kotlin/day01/Day01.kt
@@ -11,11 +11,31 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay01Part1, ::solveDay01Part2)
 }
 
-
 fun solveDay01Part1(input: List<String>): Int {
-    TODO()
+
+    val nrs = input.map { it.toInt() }
+
+    return nrs.increaseCounter()
 }
 
 fun solveDay01Part2(input: List<String>): Int {
-    TODO()
+    val nrs = input.map(String::toInt)
+
+    return nrs
+        .windowed(3, 1)
+        .map { it.sum() }
+        .increaseCounter()
 }
+
+private fun List<Int>.increaseCounter() = this.fold(
+    Pair<Int?, Int>(null, 0)
+) { acc, nextNr ->
+    if (acc.first == null) {
+        acc.copy(nextNr, 0)
+    } else if (nextNr > acc.first!!) {
+        acc.copy(nextNr, acc.second + 1)
+    } else {
+        acc.copy(nextNr, acc.second)
+    }
+
+}.second
